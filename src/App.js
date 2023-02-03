068809b8007b803bc926de747330a0f0149670a6
Akihiko Nakamura <anakamuro@earthlink.net>
2023-02-03 10:19:03 -0500

    commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react'
+import Comments from "./Comments"
 
-function App() {
+ class App extends React.Component{
+  constructor(props) {
+    super(props);
+    this.state = {
+      title: 'Dinosaurs',
+      author: 'Stealthy Stegosaurus',
+      body: 'Check out this body property!',
+      comments: ["First!", "Great post", "Hire this author now!"]
+    };
+  }
+
+
+  render(){
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+       <h1>Title: {this.state.title}</h1>
+       <h1>Author: {this.state.author}</h1>
+       <h1>Body: {this.state.body}</h1>
+       <Comments comments={this.state.comments} />
       </header>
     </div>
   );
+  }
 }
 
 export default App;
---
+++ src/Comments.js
@@ -0,0 +1,9 @@
+
+
+export default function Comments(props) {
+  return (
+    <div className="App">
+      <h1>{props.comments}</h1>
+    </div>
+  );
+}
